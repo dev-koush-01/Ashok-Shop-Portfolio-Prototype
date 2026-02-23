5754b764e328993a65798e4573d913b98fa1064b
dev-koush-01 <koushikc030@gmail.com>
2026-02-24 02:51:05 +0530

    Updated tailwind.config.cjs to replace the amber palette with shades based on #a13741

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,22 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amber: {
+          50: '#fff5f6',
+          100: '#fdecea',
+          200: '#f6c7c9',
+          300: '#f0a1a5',
+          400: '#e06b6f',
+          500: '#cf4246',
+          600: '#a13741', // primary theme color
+          700: '#7e2b2e',
+          800: '#581d1f',
+          900: '#2f0c0d',
+        }
+      }
+    },
   },
   plugins: [],
 }
